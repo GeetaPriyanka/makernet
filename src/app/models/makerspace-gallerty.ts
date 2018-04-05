6d43a860777d279d7d9d4bba63b83d5a97d045e4
Kevin Kluttz <kfkluttz@gmail.com>
2018-04-05 16:47:03 -0400

    Created initial model for MakerspaceGallery

---
+++ src/app/models/makerspace-gallerty.ts
@@ -0,0 +1,11 @@
+export class MakerspaceGallery {
+  images: Array<string>;
+  thumbnail: string;
+
+
+  constructor() {
+    this.images = new Array<string>();
+    this.thumbnail = null;
+  }
+
+}
